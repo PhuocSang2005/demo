088974ae22de3d2f182b1aa4e50fce8dc895b93a
PhuocSang2005 <sangltpps40359@gmail.com>
2024-10-29 16:51:24 +0700

    demo

---
+++ index.js
@@ -0,0 +1,18 @@
+// let name = "ABC";
+// console.log(`Tôi tên là : ${name}`);
+
+// function count(count){
+//     for( let i = 0 ; i < count; i++)
+//         console.log(`Số : ${i}`);
+// }
+// count(10);
+
+var arr = [
+    "Nguyễn Văn A", "Nguyễn Văn B", "Nguyễn Van C"
+]
+    function PrintName(arr){
+        for(let i = 0 ; i < arr.length ; i++){
+            console.log(`Ten sinh vien : ${arr[i]}`)
+        }
+    }
+    PrintName(arr);
